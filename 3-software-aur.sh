549c5e52de17c28a69f41e2df0007ac6196c912c
f4rm0r <anders.buja@gmail.com>
2021-10-16 11:48:26 +0000

    Update 3-software-aur.sh

--- 3-software-aur.sh
+++ 3-software-aur.sh
@@ -15,18 +15,27 @@ cd ~
 git clone "https://aur.archlinux.org/yay.git"
 cd ${HOME}/yay
 makepkg -si --noconfirm
-
+cd ~
+touch "$HOME/.cache/zshhistory"
+git clone "https://github.com/ChrisTitusTech/zsh"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
+ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
 
 
 PKGS=(
 'brave-bin' # Brave Browser
 'dxvk-bin' # DXVK DirectX to Vulcan
+'lightly-git'
+'lightlyshaders-git'
 'github-desktop-bin' # Github Desktop sync
 'mangohud' # Gaming FPS Counter
 'mangohud-common'
+'timeshift-bin'
+'ttf-meslo' # Nerdfont package
 'ocs-url' # install packages from websites
 'zoom' # video conferences
 'snapd' # snap store
+'autojump'
 )
 
 
