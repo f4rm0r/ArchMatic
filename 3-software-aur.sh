0b2c806079647a0183cfc5044c0e541ff2197023
f4rm0r <anders.buja@gmail.com>
2021-10-20 22:32:41 +0200

    Update 3-software-aur.sh

--- 3-software-aur.sh
+++ 3-software-aur.sh
@@ -22,33 +22,54 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerleve
 ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
 
 PKGS=(
+'autojump'
+'awesome-terminal-fonts'
 'brave-bin' # Brave Browser
 'dxvk-bin' # DXVK DirectX to Vulcan
-'lightly-git'
-'lightlyshaders-git'
 'github-desktop-bin' # Github Desktop sync
 'lightly-git'
 'lightlyshaders-git'
 'mangohud' # Gaming FPS Counter
 'mangohud-common'
-'timeshift-bin'
-'ttf-meslo' # Nerdfont package
+'nerd-fonts-fira-code'
+'nordic-darker-standard-buttons-theme'
+'nordic-darker-theme'
+'nordic-kde-git'
+'nordic-theme'
+'noto-fonts-emoji'
+'papirus-icon-theme'
+'sddm-nordic-theme-git'
 'ocs-url' # install packages from websites
 'timeshift-bin'
 'ttf-meslo' # Nerdfont package
+'ttf-droid'
+'ttf-hack'
+'ttf-roboto'
 'zoom' # video conferences
-'autojump'
-)
 
+)
 
 for PKG in "${PKGS[@]}"; do
     yay -S --noconfirm $PKG
 done
 
-# Enable and start snapstore
+cat <<EOF >> /home/$(whoami)/.config/mpv/mpv.conf
+vo=vdpau
+profile=opengl-hq
+hwdec=vdpau
+hwdec-codecs=all
+scale=ewa_lanczossharp
+cscale=ewa_lanczossharp
+interpolation
+tscale=oversample
+EOF
 
-sudo systemctl enable --now snapd.service
-sudo snap install openra
+export PATH=$PATH:~/.local/bin
+cp -r $HOME/ArchMatic/dotfiles/* $HOME/.config/
+pip install konsave
+konsave -i $HOME/ArchMatic/kde.knsv
+sleep 1
+konsave -a kde
 
 echo -e "\nDone!\n"
 exit
